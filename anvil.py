271f0e6bfb856a878287b2894f24c190548db44a
Marcel Kozlowski <37003922+marcelkoz@users.noreply.github.com>
2022-07-23 00:25:11 +0100

    anvil.py: Complete 'init' command

--- anvil.py
+++ anvil.py
@@ -1,26 +1,61 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
+import shutil
 import typing
 import sys
 import os
 
 def command_init(args: typing.List[str]):
     do_current_location = len(args) == 0
-    location = Path(os.getcwd() if do_current_location else args[0])
+    location = Path(os.getcwd() if do_current_location else args[0]).resolve()
 
-    create_config(location)
-    create_run(location)
-    create_data(location)
+    if not location.exists():
+        raise Exception(f'Location "{location}" does not exist')
+
+    config = create_config(location)
+    data = create_data(location)
+    _ = create_run(location, data, config)
 
 def create_config(location: Path):
-    pass
+    location = location / 'config'
+    location.mkdir(exist_ok=True)
 
-def create_run(location: Path):
-    pass
+    copy_item(location, 'eula.txt')
+    copy_item(location, 'server.properties')
+
+    return location
 
 def create_data(location: Path):
-    pass
+    location = location / 'data'
+    location.mkdir(exist_ok=True)
+
+    (location / 'worlds').mkdir(exist_ok=True)
+    (location / 'logs').mkdir(exist_ok=True)
+
+    return location
+
+def create_run(location: Path, data: Path, config: Path):
+    location = location / 'run'
+    location.mkdir(exist_ok=True)
+
+    link_item(config, location, 'eula.txt')
+    link_item(config, location, 'server.properties')
+    link_item(data, location, 'logs')
+    link_item(data, location, 'worlds')
+
+    return location
+
+def copy_item(destination: Path, item: str):
+    # TODO: change to anvil install location + file_name
+    file_path = Path(item)
+    shutil.copy(file_path, destination)
+
+def link_item(source: Path, destination: Path, item: str):
+    destination = destination / item
+    source = source / item
+    if not destination.exists():
+        os.symlink(source, destination)
 
 def main():
     args = sys.argv
@@ -29,8 +64,8 @@ def main():
         'init': command_init,
     }
 
-    command = args[0]
-    command_dispatch[command](args[1:])
+    command = args[1]
+    command_dispatch[command](args[2:])
 
 if __name__ == '__main__':
     main()
